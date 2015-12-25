5ace3349a1719da8b284d0de5bc6d720c9bf4751
Dmitri Pisarev <dimaip@gmail.com>
2015-12-25 14:40:11 +0300

    Fix unbound variable

--- container-files/config/init/60-install-neos.sh
+++ container-files/config/init/60-install-neos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+REPOSITORY_URL=${REPOSITORY_URL:=""}
+
 if [ -d /data/www/releases/current ] || [ -z "$REPOSITORY_URL" ]
 then
     echo "Do nothing, Neos distr is already there or REPOSITORY_URL env variable not set"
