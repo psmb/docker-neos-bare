c4f6bc84e582fb9128c44a3fd7a2a9bdf682519b
brainstorm1313 <for_spam_000@mail.ru>
2016-04-01 12:17:05 +0300

    set ulimit before start ansible

--- container-files/config/init/60-install-neos.sh
+++ container-files/config/init/60-install-neos.sh
@@ -6,6 +6,7 @@ if [ -d /data/www/releases/current ] || [ -z "$REPOSITORY_URL" ]
 then
     echo "Do nothing, Neos distr is already there or REPOSITORY_URL env variable not set"
 else
+    ulimit -n 1024
     touch i
     ansible-playbook /config/setup-neos.yml -i 'i'
 fi
